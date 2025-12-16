a8db019065bd603b7d2d3f460ac816de3e493b65
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2025-12-16 15:39:53 +0530

    [Ayush Chouhan] : added program of volume of cylinder

---
+++ gcr-codebase/program-prerequisite/VolumeOfCylinder.java
@@ -0,0 +1,9 @@
+ import java.util.Scanner;
+ public class VolumeOfCylinder{
+  public static void main(String[] args){
+    System.out.println("Enter radius and height");
+	double = Math.PI;
+	System.out.println("Volume of Cyliner = " + (pi *(radius* radius)* height);
+	}
+  }	
+ 
