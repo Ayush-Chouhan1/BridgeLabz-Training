a2b05b14a508b847293fa7ec59d3a22b18090ebb
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2025-12-17 11:02:07 +0530

    [Ayush Chouhan] : added level-1 folder

---
+++ gcr-codebase/programming-elements/level-1/calculateAge.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ public class calculateAge{
+   public void main(String[] args){
+        String name = "Harry";
+        int birthYear = 2000;
+        int currentYear = 2024;
+        int age = 2024-2000';
+        
+       System.out.println(age);
+     }
+  }
