84c5d55f31edebff41218f4a433abd524a9b7af8
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2025-12-28 19:59:53 +0530

    [Ayush]: added a program of bank account and bank account driver

--- java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/constructor/level1/CircleDriver.java
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/constructor/level1/CircleDriver.java
@@ -5,12 +5,12 @@ public class CircleDriver {
 	public static void main(String[] args) {
 
 		//initialize default constructor
-		Circle circle1 = new Circle();
-		circle1.display();
+		Circle circle1 = new Circle(34);
+		circle1.areaCircle();
 
 		//passing parameters in parameterized constructor
 		Circle circle2 = new Circle(10.5);
-		circle2.display();
+		circle2.areaCircle();
 	}
 
 }
