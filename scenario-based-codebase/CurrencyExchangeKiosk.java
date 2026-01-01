ee85a9b7994c887e8a46bfcb95b8e30d2107630d
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-01 14:39:54 +0530

    [Ayush]: added a program of currency exchange kiosk

---
+++ scenario-based-codebase/CurrencyExchangeKiosk.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+public class CurrencyExchangeKiosk {
+
+    /*  18. Currency Exchange Kiosk 💱
+        Design a currency converter:
+        ● Take INR amount and target currency.
+        ● Use a switch to apply the correct rate.
+        ● Ask if the user wants another conversion (do-while).*/
+
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char choice;
+
+        do {
+            System.out.print("Enter amount in INR: ");
+            double inr = sc.nextDouble();
+
+            System.out.println("Choose target currency:");
+            System.out.println("1. USD");
+            System.out.println("2. EUR");
+            System.out.println("3. GBP");
+
+            System.out.print("Enter option: ");
+            int option = sc.nextInt();
+
+            double convertedAmount = 0;
+
+            switch (option) {
+                case 1:
+                    convertedAmount = inr * 0.012; // USD
+                    System.out.println("Amount in USD: " + convertedAmount);
+                    break;
+
+                case 2:
+                    convertedAmount = inr * 0.011; // EUR
+                    System.out.println("Amount in EUR: " + convertedAmount);
+                    break;
+
+                case 3:
+                    convertedAmount = inr * 0.0095; // GBP
+                    System.out.println("Amount in GBP: " + convertedAmount);
+                    break;
+
+                default:
+                    System.out.println("Invalid currency option");
+            }
+
+            System.out.print("Do you want another conversion? (y/n): ");
+            choice = sc.next().charAt(0);
+
+        } while (choice == 'y' || choice == 'Y');
+
+        System.out.println("Thank you for using Currency Exchange Kiosk");
+        sc.close();
+    }
+}
