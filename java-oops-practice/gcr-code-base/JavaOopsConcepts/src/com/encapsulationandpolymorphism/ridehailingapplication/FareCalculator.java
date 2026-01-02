ce7047f6bb03f52f83059bd45366d1f2a868c5e3
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-02 21:00:53 +0530

    [Ayush]: added a program of ride hail application

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulationandpolymorphism/ridehailingapplication/FareCalculator.java
@@ -0,0 +1,17 @@
+package com.encapsulationandpolymorphism.ridehailingapplication;
+
+import java.util.List;
+
+class FareCalculator {
+
+    public static void calculateFares(List<Vehicle> vehicles, double distance) {
+
+        for (Vehicle v : vehicles) {
+            v.getVehicleDetails();
+            double fare = v.calculateFare(distance);
+            System.out.println("Distance: " + distance + " km");
+            System.out.println("Fare: " + fare);
+            System.out.println("---------------------------");
+        }
+    }
+}
