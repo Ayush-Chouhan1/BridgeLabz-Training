ed712323f0c4c74963cd038b137d265549afc209
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-17 22:55:33 +0530

    [Ayush]: modified the program of smart warehouse

--- java-collection-practice/gcr-code-base/com/src/com/generics/smartwarehousemanagementsystem/Electronics.java
+++ java-collection-practice/gcr-code-base/com/src/com/generics/smartwarehousemanagementsystem/Electronics.java
@@ -1,5 +1,13 @@
 package com.generics.smartwarehousemanagementsystem;
 
-public class Electronics {
+public class Electronics extends WarehouseItem{
 
+	public Electronics(String name, double price) {
+		super(name, price);
+	}
+	
+	public String getCategory() {
+		return "Electronics";
+	}
+	
 }
--- java-collection-practice/gcr-code-base/com/src/com/generics/smartwarehousemanagementsystem/Furniture.java
+++ java-collection-practice/gcr-code-base/com/src/com/generics/smartwarehousemanagementsystem/Furniture.java
@@ -1,5 +1,13 @@
 package com.generics.smartwarehousemanagementsystem;
 
-public class Furniture {
+public class Furniture extends WarehouseItem {
 
+	public Furniture(String name, double price) {
+		super(name, price);
+	}
+	
+	public String getCategory() {
+		return "Furniture";
+	}
+	
 }
