0f4dafde2fb837889df4c4e1963e2e030d50b96d
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-18 00:00:01 +0530

    [Ayush]: added a program of course management system

---
+++ java-collection-practice/gcr-code-base/com/src/com/coursemanagementsystem/Course.java
@@ -0,0 +1,62 @@
+package com.coursemanagementsystem;
+import java.util.List;
+
+abstract class CourseType {
+
+    public abstract String getEvaluationType();
+}
+
+class ExamCourse extends CourseType {
+    
+    public String getEvaluationType() {
+        return "Exam Based";
+    }
+}
+
+class AssignmentCourse extends CourseType {
+    
+    public String getEvaluationType() {
+        return "Assignment Based";
+    }
+}
+
+class ResearchCourse extends CourseType {
+    
+    public String getEvaluationType() {
+        return "Research Based";
+    }
+}
+
+class Course<T extends CourseType> {
+
+    private String courseName;
+    private T courseType;
+
+    public Course(String courseName, T courseType) {
+        this.courseName = courseName;
+        this.courseType = courseType;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public T getCourseType() {
+        return courseType;
+    }
+
+    @Override
+    public String toString() {
+        return "Course name=" + courseName + " , evaluation= " + courseType.getEvaluationType() ;
+    }
+}
+
+class CourseUtil {
+
+    public static void displayCourses(List<? extends CourseType> courseTypes) {
+
+        for (CourseType ct : courseTypes) {
+            System.out.println("evaluation Type " + ct.getEvaluationType());
+        }
+    }
+}
