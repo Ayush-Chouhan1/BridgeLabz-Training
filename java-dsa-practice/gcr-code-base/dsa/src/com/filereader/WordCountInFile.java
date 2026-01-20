3f44a5118f98cbf29652390ec75995c9eb0b513e
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-20 16:30:20 +0530

    [Ayush]: modified the program of file reader

---
+++ java-dsa-practice/gcr-code-base/dsa/src/com/filereader/WordCountInFile.java
@@ -0,0 +1,41 @@
+package com.filereader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WordCountInFile {
+
+    public static void main(String[] args) {
+
+        String fileName = "input.txt";
+        String targetWord = "java";
+        int count = 0;
+
+        try {
+            FileReader fr = new FileReader(fileName);
+            BufferedReader br = new BufferedReader(fr);
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+
+                String[] words = line.split(" ");
+
+                for (int i = 0; i < words.length; i++) {
+                    if (words[i].equals(targetWord)) {
+                        count++;
+                    }
+                }
+            }
+
+            br.close();
+        }
+        catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+
+        System.out.println("Occurrences of '" + targetWord + "': " + count);
+    }
+}
+  
