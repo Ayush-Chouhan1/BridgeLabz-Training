65216650692d648845874bc4ca5b8ee064e3da35
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-20 16:12:28 +0530

    [Ayush]: added a program of file reader

---
+++ java-dsa-practice/gcr-code-base/dsa/src/com/ReadFileLineByLine.java
@@ -0,0 +1,29 @@
+package com.filereader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileLineByLine {
+
+    public static void main(String[] args) {
+
+        try {
+            FileReader fileReader = new FileReader("input.txt");
+            BufferedReader br = new BufferedReader(fileReader);
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close(); 
+        }
+        catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
+
+
