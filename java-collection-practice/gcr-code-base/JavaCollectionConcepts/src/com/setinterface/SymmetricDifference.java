13cf18974c95b395ac9d50606c9078779ed36fac
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-24 12:00:01 +0530

    [Ayush]: added a program of symmetric difference

---
+++ java-collection-practice/gcr-code-base/JavaCollectionConcepts/src/com/setinterface/SymmetricDifference.java
@@ -0,0 +1,36 @@
+package com.setinterface;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SymmetricDifference {
+
+    public static void main(String[] args) {
+
+        // Create two sets
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+
+        // Union of sets
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+
+        // Intersection of sets
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+
+        // Symmetric Difference = Union - Intersection
+        union.removeAll(intersection);
+
+        // Display result
+        System.out.println("Set1: " + set1);
+        System.out.println("Set2: " + set2);
+        System.out.println("Symmetric Difference: " + union);
+    }
+}
