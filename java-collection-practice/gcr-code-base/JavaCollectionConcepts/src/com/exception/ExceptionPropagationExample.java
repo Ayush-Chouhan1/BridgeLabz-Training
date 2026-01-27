bfc1a0294e2778bd8407b4bbf3c2db49b2985e0e
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-27 13:09:25 +0530

    [Ayush]: added a program of exception propagation example

---
+++ java-collection-practice/gcr-code-base/JavaCollectionConcepts/src/com/exception/ExceptionPropagationExample.java
@@ -0,0 +1,21 @@
+package com.exception;
+public class ExceptionPropagationExample {
+
+    static void method1() {
+        int result = 10 / 0;   // ArithmeticException
+    }
+
+    static void method2() {
+        method1();            // Exception propagates
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            method2();        // Exception reaches main
+
+        } catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
+}
