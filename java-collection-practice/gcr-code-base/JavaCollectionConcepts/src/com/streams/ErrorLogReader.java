45f52e67102c323b77a2bb1453d35b91c9eae701
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-27 12:37:14 +0530

    [Ayush]: added a program of Error log reader

---
+++ java-collection-practice/gcr-code-base/JavaCollectionConcepts/src/com/streams/ErrorLogReader.java
@@ -0,0 +1,28 @@
+package com.streams;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ErrorLogReader {
+
+    public static void main(String[] args) {
+
+        String filePath = "largefile.txt"; 
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+            String line;
+
+            // Read file line by line
+            while ((line = br.readLine()) != null) {
+
+                if (line.toLowerCase().contains("error")) {
+                    System.out.println(line);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println(" Error reading file: " + e.getMessage());
+        }
+    }
+}
