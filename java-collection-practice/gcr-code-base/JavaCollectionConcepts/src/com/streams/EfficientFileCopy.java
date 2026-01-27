55f53b31344416571f6ba4ecc94e9a519cf13862
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-27 12:10:28 +0530

    [Ayush]: added a program of Efficent file copy

---
+++ java-collection-practice/gcr-code-base/JavaCollectionConcepts/src/com/streams/EfficientFileCopy.java
@@ -0,0 +1,68 @@
+package com.streams;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.*;
+import java.io.IOException;
+
+public class EfficientFileCopy {
+	
+	    private static final int BUFFER_SIZE = 4096; // 4 KB
+
+	    public static void main(String[] args) {
+
+	        String sourceFile = "largefile.dat";
+	        String unbufferedDest = "copy_unbuffered.dat";
+	        String bufferedDest = "copy_buffered.dat";
+
+	        copyUsingUnbufferedStreams(sourceFile, unbufferedDest);
+	        copyUsingBufferedStreams(sourceFile, bufferedDest);
+	    }
+
+	    //  Unbuffered copy
+	    public static void copyUsingUnbufferedStreams(String src, String dest) {
+	        long startTime = System.nanoTime();
+
+	        try (FileInputStream fis = new FileInputStream(src);
+	             FileOutputStream fos = new FileOutputStream(dest)) {
+
+	            byte[] buffer = new byte[BUFFER_SIZE];
+	            int bytesRead;
+
+	            while ((bytesRead = fis.read(buffer)) != -1) {
+	                fos.write(buffer, 0, bytesRead);
+	            }
+
+	        } catch (IOException e) {
+	            System.out.println("Unbuffered copy failed.");
+	            return;
+	        }
+
+	        long endTime = System.nanoTime();
+	        System.out.println("Unbuffered Time: " + (endTime - startTime) / 1_000_000 + " ms");
+	    }
+
+	    // Buffered copy
+	    public static void copyUsingBufferedStreams(String src, String dest) {
+	        long startTime = System.nanoTime();
+
+	        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(src));
+	             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(dest))) {
+
+	            byte[] buffer = new byte[BUFFER_SIZE];
+	            int bytesRead;
+
+	            while ((bytesRead = bis.read(buffer)) != -1) {
+	                bos.write(buffer, 0, bytesRead);
+	            }
+
+	        } catch (IOException e) {
+	            System.out.println("Buffered copy failed.");
+	            return;
+	        }
+
+	        long endTime = System.nanoTime();
+	        System.out.println("Buffered Time: " + (endTime - startTime) / 1_000_000 + " ms");
+	    }
+	}
+
