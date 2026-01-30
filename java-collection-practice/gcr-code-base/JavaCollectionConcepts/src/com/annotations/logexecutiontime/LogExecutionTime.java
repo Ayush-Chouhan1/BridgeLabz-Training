16cd82d018284150901425ea2c3eef733e85c278
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-30 23:04:45 +0530

    [Ayush]: added a program of log execution time

---
+++ java-collection-practice/gcr-code-base/JavaCollectionConcepts/src/com/annotations/logexecutiontime/LogExecutionTime.java
@@ -0,0 +1,8 @@
+package com.annotations.logexecutiontime;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface LogExecutionTime {
+}
