721b340ae238c478708f57c1d201fb7b004f8742
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-30 23:09:22 +0530

    [Ayush]: added a program of repetable annotations

---
+++ java-collection-practice/gcr-code-base/JavaCollectionConcepts/src/com/annotations/repetableannotation/BugReports.java
@@ -0,0 +1,9 @@
+package com.annotations.repetableannotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BugReports {
+	BugReport[] value();
+}
---
+++ java-collection-practice/gcr-code-base/JavaCollectionConcepts/src/com/annotations/repetableannotation/BugTracker.java
@@ -0,0 +1,10 @@
+package com.annotations.repetableannotation;
+
+public class BugTracker {
+
+	@BugReport(description = "Null pointer issue")
+	@BugReport(description = "Performance issue")
+	public void process() {
+		System.out.println("Processing task");
+	}
+}
