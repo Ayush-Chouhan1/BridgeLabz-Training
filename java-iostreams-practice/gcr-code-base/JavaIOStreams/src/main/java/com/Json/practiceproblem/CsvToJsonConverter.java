cc90d92fb6386ba0b42da86ff88a32f0411bcf33
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-31 16:14:37 +0530

    [ayush]: added a program of filter by age

---
+++ java-iostreams-practice/gcr-code-base/JavaIOStreams/src/main/java/com/Json/practiceproblem/CsvToJsonConverter.java
@@ -0,0 +1,30 @@
+package com.Json.practiceproblem;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+public class CsvToJsonConverter {
+
+	public static void main(String[] args) throws Exception {
+
+		// CSV mapper and schema (with header)
+		CsvMapper csvMapper = new CsvMapper();
+		CsvSchema schema = CsvSchema.emptySchema().withHeader();
+
+		// Read CSV into List of Maps
+		List<Object> csvData = csvMapper.readerFor(Map.class).with(schema).readValues(new File("src/main/java/com/JavaIOStreams/countcsvrows/employees.csv"))
+				.readAll();
+
+		// Convert to JSON
+		ObjectMapper jsonMapper = new ObjectMapper();
+		String json = jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(csvData);
+
+		// Print JSON
+		System.out.println(json);
+	}
+}
