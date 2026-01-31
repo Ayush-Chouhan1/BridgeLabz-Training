d0c9ad4a0dc5055ad06d5df88e3b68fbc1b4ffbc
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-31 16:20:39 +0530

    [Ayush]: added a program of validate json using jackson

---
+++ java-iostreams-practice/gcr-code-base/JavaIOStreams/src/main/java/com/Json/practiceproblem/ValidateJsonUsingJackson.java
@@ -0,0 +1,24 @@
+package com.Json.practiceproblem;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ValidateJsonUsingJackson {
+
+	public static void main(String[] args) {
+
+		// JSON string to validate
+		String json = "{ \"name\": \"Ayush\", \"age\": 22 }";
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			// Try parsing JSON
+			mapper.readTree(json);
+
+			System.out.println("Valid JSON");
+
+		} catch (Exception e) {
+			System.out.println("Invalid JSON");
+		}
+	}
+}
