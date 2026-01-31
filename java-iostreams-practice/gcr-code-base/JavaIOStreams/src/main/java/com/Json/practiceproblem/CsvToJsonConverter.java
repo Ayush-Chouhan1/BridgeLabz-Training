bd5718e294472cb3546551553c6447df333dae51
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-01-31 16:36:04 +0530

    [Ayush]: modified a program of csv to json converter

--- java-iostreams-practice/gcr-code-base/JavaIOStreams/src/main/java/com/Json/practiceproblem/CsvToJsonConverter.java
+++ java-iostreams-practice/gcr-code-base/JavaIOStreams/src/main/java/com/Json/practiceproblem/CsvToJsonConverter.java
@@ -1,7 +1,28 @@
 package com.Json.practiceproblem;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ValidateJsonUsingJackson {
+
+	public static void main(String[] args) {
+
+		// JSON string to validate
+		String json = "{ \"name\": \"Vikash\", \"age\": 22 }";
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			// Try parsing JSON
+			mapper.readTree(json);
+
+			System.out.println("Valid JSON");
+
+		} catch (Exception e) {
+			System.out.println("Invalid JSON");
+		}
+	}
+}
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CarToJson {
