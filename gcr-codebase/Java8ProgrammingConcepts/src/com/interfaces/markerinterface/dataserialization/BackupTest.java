9730b557085aff508cd397264593bc7077e7c010
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-02-02 17:10:24 +0530

    [Ayush]: added a program of data serialization

---
+++ gcr-codebase/Java8ProgrammingConcepts/src/com/interfaces/markerinterface/dataserialization/BackupTest.java
@@ -0,0 +1,11 @@
+package com.interfaces.markerinterface.dataserialization;
+
+public class BackupTest {
+	public static void main(String[] args) {
+		PatientRecord record = new PatientRecord(101, "Ayush");
+		SessionCache cache = new SessionCache("ABC123");
+
+		BackupService.backup(record);
+		BackupService.backup(cache);
+	}
+}
---
+++ gcr-codebase/Java8ProgrammingConcepts/src/com/interfaces/markerinterface/dataserialization/PatientRecord.java
@@ -0,0 +1,11 @@
+package com.interfaces.markerinterface.dataserialization;
+
+public class PatientRecord implements BackupEligible {
+	int id;
+	String name;
+
+	public PatientRecord(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+}
