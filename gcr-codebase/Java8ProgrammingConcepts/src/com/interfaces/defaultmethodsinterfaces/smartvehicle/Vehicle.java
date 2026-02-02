020f7f9070cff0ab43d30392738355bb190c2770
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-02-02 17:07:00 +0530

    [Ayush]: added a program of smart vehicle

---
+++ gcr-codebase/Java8ProgrammingConcepts/src/com/interfaces/defaultmethodsinterfaces/smartvehicle/Vehicle.java
@@ -0,0 +1,10 @@
+package com.interfaces.defaultmethodsinterfaces.smartvehicle;
+
+public interface Vehicle {
+	void displaySpeed();
+
+	// New feature added safely
+	default void displayBatteryStatus() {
+		System.out.println("Battery status not available");
+	}
+}
