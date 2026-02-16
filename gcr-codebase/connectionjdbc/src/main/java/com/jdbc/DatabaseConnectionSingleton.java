59535f5b0a4c3daa15adaf946df3bd928735e075
Ayush-Chouhan1 <ayushraochouhan@gmail.com>
2026-02-16 13:04:08 +0530

    [Ayush]: added a program of database connection singleton

---
+++ gcr-codebase/connectionjdbc/src/main/java/com/jdbc/DatabaseConnectionSingleton.java
@@ -0,0 +1,33 @@
+package com.jdbc;
+
+import java.sql.*;
+
+public class DatabaseConnectionSingleton {
+
+	private static DatabaseConnectionSingleton instance;
+	private Connection connection;
+	private static final String URL = "jdbc:mysql://localhost:3306/mydb";
+	private static final String USER = "root";
+	private static final String PASSWORD = "your_password";
+
+	private DatabaseConnectionSingleton() throws SQLException {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (ClassNotFoundException e) {
+			throw new SQLException("Database Driver not found", e);
+		}
+
+	}
+
+	public static DatabaseConnectionSingleton getInstance() throws SQLException {
+		if (instance == null || instance.getConnection().isClosed()) {
+			instance = new DatabaseConnectionSingleton();
+		}
+		return instance;
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+}
